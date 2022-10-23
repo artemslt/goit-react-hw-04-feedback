a83fa753e4e7c279afbbbf4152df313cb92bf143
Artem <ppt.artem@gmail.com>
2022-10-23 21:12:17 +0300

    hw complite

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,56 +1,57 @@
+import { useState } from 'react';
+
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
-import { Component } from 'react';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class FeedbackCard extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
+export const FeedbackCard = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const options = { good, neutral, bad };
+    const total = good + neutral + bad;
+    const positivePercentage = ((good / total) * 100).toFixed(0);
+
+    const onLeaveFeedback = e => {
+        switch (e.target.value) {
+            case 'good':
+                setGood(good + 1);
+                break;
+            case 'neutral':
+                setNeutral(neutral + 1);
+                break;
+            case 'bad':
+                setBad(bad + 1);
+                break;
+
+            default:
+                break;
+        }
     };
 
-    onLeaveFeedback = e => {
-        const currentBtnValue = e.currentTarget.value;
-        this.setState(prevState => ({
-            [currentBtnValue]: prevState[currentBtnValue] + 1,
-        }));
-    };
-
-    countTotalFeedback = () =>
-        Object.values(this.state).reduce((a, b) => a + b, 0);
-
-    countPositiveFeedbackPercentage = () => {
-        const total = this.countTotalFeedback();
-        const positivePercentage = ((this.state.good / total) * 100).toFixed(0);
-        return isNaN(positivePercentage) ? '0' : positivePercentage;
-    };
-
-    render() {
-        const { good, neutral, bad } = this.state;
-        return (
-            <>
-                <Section title="Please live feedback">
-                    <FeedbackOptions
-                        options={Object.keys(this.state)}
-                        onLeaveFeedback={this.onLeaveFeedback}
-                    ></FeedbackOptions>
-                </Section>
-                <Section title="Statistics">
-                    {this.countTotalFeedback() ? (
-                        <Statistics
-                            good={good}
-                            neutral={neutral}
-                            bad={bad}
-                            total={this.countTotalFeedback()}
-                            positivePercentage={this.countPositiveFeedbackPercentage()}
-                        ></Statistics>
-                    ) : (
-                        <Notification messege="There is no feedback" />
-                    )}
-                </Section>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <Section title="Please live feedback">
+                <FeedbackOptions
+                    options={Object.keys(options)}
+                    onLeaveFeedback={onLeaveFeedback}
+                ></FeedbackOptions>
+            </Section>
+            <Section title="Statistics">
+                {total ? (
+                    <Statistics
+                        good={good}
+                        neutral={neutral}
+                        bad={bad}
+                        total={total}
+                        positivePercentage={positivePercentage}
+                    ></Statistics>
+                ) : (
+                    <Notification messege="There is no feedback" />
+                )}
+            </Section>
+        </>
+    );
+};
